2bf77d4686ff7a3390ea009fef8dafcac0c25d90
venkat <venkey402@gmail.com>
2024-08-19 11:22:40 +0530

    added extent reporter and rerun features

--- src/test/java/testRunner/TestRunner_failedRun.java
+++ src/test/java/testRunner/TestRunner_failedRun.java
@@ -5,7 +5,7 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src\\test\\java\\features", 
+@CucumberOptions(features = "src\\test\\java\\features\\failed.txt", 
 glue = { "stepDef" , "hooks" }, 
 plugin = { "pretty",
 		"html:target\\CucumberReports\\index.html",
@@ -16,7 +16,7 @@ plugin = { "pretty",
 //,tags = "@TC_03"
 
 )
-public class TestRunner extends AbstractTestNGCucumberTests {
+public class TestRunner_failedRun extends AbstractTestNGCucumberTests {
 
 	@DataProvider(parallel = false)
 	public Object[][] scenarios() {
